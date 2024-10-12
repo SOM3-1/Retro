5d07f7f9a7979258fb209a7ead33761ab54e4a27
SOM3-1 <dush.gowda@gmail.com>
2024-10-12 14:31:58 -0500

    Adding router

--- src/App.js
+++ src/App.js
@@ -1,8 +1,17 @@
 import './App.css';
+import { Ps2 } from './components/virtualConsole/Ps2';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 function App() {
   return (
     <div className="App">
-      Hello
+    <Router>
+        <Routes>
+        <Route path="/" element={<div>Hello</div>} />
+
+          <Route path="/console" element={<Ps2 />} />
+          <Route path="/games" element={<Ps2 />} />
+        </Routes>
+      </Router>
     </div>
   );
 }
